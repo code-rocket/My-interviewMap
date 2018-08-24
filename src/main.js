c426ba71b55fca996fa7538ed1d5cafc07ea4a5b
xilanhuadechuntian <Lensgcx@163.com>
2018-08-24 16:26:19 +0800

    main
    main

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './assets/front.scss'
+/** 基础 - 四则运算符 **/
+// import './JS/operation/operation';
+/** 判断两个值 / 对象相等 **/
+// import './JS/judge/judgeEqual ';
+
+
+
+
+
+{
+    window.onload = function () {
+        document.getElementById('interviewMap').innerHTML = 'InterviewMap';
+    }
+}
