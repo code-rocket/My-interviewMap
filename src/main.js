b344829eb90fc379ba9ec14d689afc2e33338233
xilanhuadechuntian <Lensgcx@163.com>
2018-08-27 09:12:16 +0800

    main
    main

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import './assets/front.scss'
 /** 基础 - 四则运算符 **/
 // import './JS/operation/operation';
+
 /** 判断两个值 / 对象相等 **/
 // import './JS/judge/judgeEqual ';
 
-
+/** instanceof **/
+// import './JS/base/instanceof';
 
 
 
