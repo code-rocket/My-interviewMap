a0a23e21621d96be11d1e23900715def0d7504d2
xilanhuadechuntian <Lensgcx@163.com>
2018-08-28 23:21:03 +0800

    build utils

--- build/utils.js
+++ build/utils.js
@@ -34,3 +34,29 @@ exports.createNotifierCallback = () => {
         })
     }
 };
+
+/**
+ * 多个数组合并
+ * @param arr1
+ * @param arr2
+ * @param arr3.......
+ * @returns {*}
+ */
+exports.arrayConcat = (arr1, arr2, arr3) => {
+    if (arguments.length <= 1) {
+        return false;
+    }
+    var concat_ = function (arr1, arr2) {
+        var arr = arr1.concat();
+        for (var i = 0; i < arr2.length; i++) {
+            arr.indexOf(arr2[i]) === -1 ? arr.push(arr2[i]) : 0;
+        }
+        return arr;
+    }
+    var result = concat_(arr1, arr2);
+    for (var i = 2; i < arguments.length; i++) {
+        result = concat_(result, arguments[i]);
+    }
+    return result;
+};
+
