524517f2765539c677ed0128956fecc26dabd003
xilanhuadechuntian <Lensgcx@163.com>
2018-08-28 23:21:09 +0800

    webpack.base.conf
    webpack.base.conf

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,38 +1,45 @@
 let path = require('path');
-let webpack = require('webpack');
 
 function resolve(dir) {
     return path.join(__dirname, '..', dir)
 }
 
 module.exports = {
-    entry: './src/main.js',
+    // 配置入口
+    entry: {
+
+    },
+    // 配置出口
     output: {
-        path: path.resolve(__dirname, 'dist'), //输出目录的配置，模板、样式、脚本、图片等资源的路径配置都相对于它
-        // path: '/',
-        publicPath: '/dist/',
-        filename: "bundle.js"
+        path: path.join(__dirname, "../dist/"),
+        filename: 'static/js/[name].[hash:7].js',
+        publicPath: '/',
     },
     devServer: {},
     //加载器
     module: {
+
         rules: [
+            {test: /\.(htm|html)/, loader: 'html-loader'},
+            {test: /\.(tpl|ejs)$/, loader: 'ejs-loader'},
             {
-                test: /\.html$/,
-                loader: "raw-loader"
-            },
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader'
+                test: /\.(scss|sass|css)$/,  // pack sass and css files
+                // loader: ExtractTextPlugin.extract({
+                //         fallback: "style-loader",
+                //         use: "css-loader!postcss-loader!sass-loader"
+                //     }
+                // )
+                use: [
+                    'style-loader',
+                    {loader: 'css-loader', options: {importLoaders: 2}},  //2代表css-loader后还需要几个loader
+                    {loader: 'postcss-loader', options: {plugins: [require("autoprefixer")("last 100 versions")]}},
+                    'sass-loader'
+                ]
             },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            },
-            {
-                test: /\.scss$/,
-                loader: 'style-loader!css-loader!sass-loader'
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
@@ -41,9 +48,14 @@ module.exports = {
             {
                 //图片加载器，雷同file-loader，更适合图片，可以将较小的图片转成base64，减少http请求
                 //如下配置，将小于8192byte的图片转成base64码
-                test: /\.(png|jpg|gif)$/,
-                loader: 'url-loader?limit=8192&name=images/[hash].[ext]'
-            }
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader?limit=8192&name=images/[hash].[ext]',
+                options: {
+                    limit: 10000,
+                    name: 'static/img/[name].[hash:7].[ext]'
+                }
+            },
+            {test: /\.hbs$/, loader: "handlebars-loader"}
         ]
     },
     plugins: [],
--- src/pages/JS/base/this.js
+++ src/pages/JS/base/this.js
@@ -5,12 +5,4 @@
         console.log(foo)
     }()) // -> ƒ foo() { foo = 10 ; console.log(foo) }
 
-
-
-    // multi(2)(3)(4)=24?
-
-    function multi(x) {
-        return x * multi(x);
-    }
-    multi()
 }
