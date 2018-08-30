413d5ac21abcaeaab7684a53aaf0b86b6e57d49d
xilanhuadechuntian <Lensgcx@163.com>
2018-08-30 12:24:05 +0800

    home
    home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,11 @@
+import {loadHtml} from "../../utils/index";
 import './index.scss'
+import '../../components/side-menu/side-menu.scss'
 
-console.log('home');
-console.log('首页的js运行了～～');
+$(document).ready(readyHandler);
 
-
+function readyHandler() {
+    console.log('首页的脚本js运行了～～');
+    $('#interviewMap').css('color', 'red');
+    loadHtml("#side-menu", './components/side-menu.html');
+}
