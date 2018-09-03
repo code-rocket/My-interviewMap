6afc94934ced3764b75e05459bafc1a0f5e82452
xilanhuadechuntian <Lensgcx@163.com>
2018-09-03 21:58:15 +0800

    main
    main

--- src/main.js
+++ src/main.js
@@ -1,30 +1,13 @@
 import $ from "jquery";
-import './vendor/bootstrap/js/bootstrap';
+import 'bootstrap';
 import './assets/styles/main.scss';
 
-
-import './components/side-menu/side-menu'
-import {loadHtml} from './utils/index'
+import './components/side-menu/side-menu';
 
 console.log('main.js loaded');
-
 $(document).ready(() => {
-	console.log('jquery works');
-	// loadHtml("#page-container", 0);
-	// menuClickHandler("#page-container");
+    console.log('jquery works');
+
 });
 
-/**
- * menuClickHandler
- * @param target
- */
-function menuClickHandler(target) {
-	const sideID = "#side-menu";
-	console.log($(sideID).find(">li"));
-	console.log('12312312312');
-	$(sideID).find(">li.side-menu-item").on("click", function () {
-		console.log('click');
-		let url = $(this).find(">a").attr("data-href");
-		loadHtml(target, url);
-	});
-}
+
