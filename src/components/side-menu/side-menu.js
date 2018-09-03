ae68fa1051f8709f3aebb37acfddbcc4280826b4
xilanhuadechuntian <Lensgcx@163.com>
2018-09-03 21:47:24 +0800

    side-menu script
    side-menu script

--- src/components/side-menu/side-menu.js
+++ src/components/side-menu/side-menu.js
@@ -1,10 +1,44 @@
-$(document).ready(function () {
-    console.log('加载侧边栏脚本js～～');
+import {Hbs} from '../../utils/index';
 
-
-});
-
+// const Handlebars = require('handlebars/dist/handlebars.min');
 const menuData = require('./side-menu.json');
 const sideMenuTemp = require('./side-menu.hbs');
-const sideMenuTempFn = sideMenuTemp(menuData);//编译
-module.exports = sideMenuTempFn;
+
+$(document).ready(() => {
+	console.log('side-menu works');
+
+	let HBS = new Hbs();
+	HBS.directImport(sideMenuTemp, '#sidebar-wrapper', menuData);//导入侧边栏
+
+	// HBS.compileImport('#side-menu-temp', '#side-menu', menuData).then(() => {
+	//
+	// });
+
+	let trigger = $('.hamburger'), overlay = $('.overlay'), isClosed = false;
+
+	trigger.click(function () {
+		hamburger_cross();
+	});
+
+	function hamburger_cross() {
+
+		if (isClosed === true) {
+			overlay.hide();
+			trigger.removeClass('is-open');
+			trigger.addClass('is-closed');
+			isClosed = false;
+		} else {
+			overlay.show();
+			trigger.removeClass('is-closed');
+			trigger.addClass('is-open');
+			isClosed = true;
+		}
+	}
+
+	$('[data-toggle="offcanvas"]').click(function () {
+		console.log(123213)
+		console.log($('#wrapper'));
+		$('#wrapper').toggleClass('toggled');
+	});
+
+});
