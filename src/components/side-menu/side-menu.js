d9c8966f94e42cf482bfacb7c8726e7aba04bf5a
xilanhuadechuntian <Lensgcx@163.com>
2018-09-05 22:27:53 +0800

    side-menu
    side-menu

--- src/components/side-menu/side-menu.js
+++ src/components/side-menu/side-menu.js
@@ -1,44 +1,61 @@
-import {Hbs} from '../../utils/index';
+import {Hbs, getPageConfig} from '../../utils/index';
+import pageConfig from '../../page.config';
+import status from '../../status';
+import Cookies from 'js-cookie';
 
 // const Handlebars = require('handlebars/dist/handlebars.min');
 const menuData = require('./side-menu.json');
 const sideMenuTemp = require('./side-menu.hbs');
 
 $(document).ready(() => {
-	console.log('side-menu works');
+    console.log('side-menu works');
 
-	let HBS = new Hbs();
-	HBS.directImport(sideMenuTemp, '#sidebar-wrapper', menuData);//导入侧边栏
+    let HBS = new Hbs();
+    HBS.directImport(sideMenuTemp, '#sidebar-wrapper', menuData);//导入侧边栏
 
-	// HBS.compileImport('#side-menu-temp', '#side-menu', menuData).then(() => {
-	//
-	// });
+    // HBS.compileImport('#side-menu-temp', '#side-menu', menuData).then(() => {
+    //
+    // });
 
-	let trigger = $('.hamburger'), overlay = $('.overlay'), isClosed = false;
 
-	trigger.click(function () {
-		hamburger_cross();
-	});
+    //  sidebar dom
+    let sidebarNav = $('#sidebar-wrapper .sidebar-nav');
 
-	function hamburger_cross() {
+    //add active class for current navLink
+    initActive();
 
-		if (isClosed === true) {
-			overlay.hide();
-			trigger.removeClass('is-open');
-			trigger.addClass('is-closed');
-			isClosed = false;
-		} else {
-			overlay.show();
-			trigger.removeClass('is-closed');
-			trigger.addClass('is-open');
-			isClosed = true;
-		}
-	}
+    sidebarNav[0].addEventListener('click', menuClickHandler);
 
-	$('[data-toggle="offcanvas"]').click(function () {
-		console.log(123213)
-		console.log($('#wrapper'));
-		$('#wrapper').toggleClass('toggled');
-	});
 
+    function initActive() {
+        const nav_link = sidebarNav.find("a.side-menu-link");
+        const nav_list = Array.prototype.slice.call(nav_link);//get a.nav-link array
+        const index = Cookies.get('menu_status').activeIndex || status.menu_status.activeIndex;
+        nav_list.forEach((a, i) => {
+            $(a).removeClass('active');
+            if (i === index) {
+                $(a).addClass('active');
+            }
+        });
+    }
+
+    function menuClickHandler(e) {
+        e = e || window.event;
+        const target = e.target || e.srcElement;
+        if (!!target && target.className.toLowerCase() === 'side-menu-link') {
+            const url = $(target).attr("data-href");
+            const page_name = $(target).attr("name");
+            let nav_link = Array.prototype.slice.call($(this).find("a.side-menu-link"));//get a.nav-link array
+            const index = Array.prototype.indexOf.call(nav_link, target);//a标签 下标索引
+            nav_link.forEach((a, i) => {
+                $(a).removeClass('active');
+                if (i === index) {
+                    $(a).addClass('active');
+                }
+            });
+            const pageConf = getPageConfig(pageConfig.pages, page_name);
+            Cookies.set('page_info', pageConf);
+            window.location.href = url;
+        }
+    }
 });
