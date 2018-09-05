5415a371b03c4c901a38f70205787bfc3cc2af31
xilanhuadechuntian <Lensgcx@163.com>
2018-09-05 22:28:02 +0800

    page.config
    page.config

--- src/page.config.js
+++ src/page.config.js
@@ -1,42 +1,45 @@
 const pageConfig = {
-	command: {
-		css: [],
-		js: [
-			{name: 'main', path: './src/main.js'}
-		],
-	},
-	pages: [
-		{
-			name: 'home',
-			template: 'src/pages/home/home.hbs',
-			output: 'dist/index.html',
-			jsEntry: 'src/pages/home/home.js',
-			content: {
-				title: 'My interviewMap | Home',
-				description: 'Home Page'
-			},
-		},
-		{
-			name: 'about',
-			template: 'src/pages/about/about.hbs',
-			output: 'dist/pages/about.html',
-			jsEntry: 'src/pages/about/about.js',
-			content: {
-				title: 'My interviewMap | About',
-				description: 'About Page'
-			},
-		},
-		{
-			name: 'test',
-			template: 'src/pages/test/test.hbs',
-			output: 'dist/pages/test.html',
-			jsEntry: '',
-			content: {
-				title: 'My interviewMap | Test',
-				description: 'Test Page'
-			},
-		}
-	]
+    command: {
+        css: [],
+        js: [
+            {name: 'main', path: './src/main.js'}
+        ],
+    },
+    pages: [
+        {
+            name: 'home',
+            template: 'src/pages/home/home.hbs',
+            output: 'dist/index.html',
+            jsEntry: 'src/pages/home/home.js',
+            content: {
+                title: 'My interviewMap | Home',
+                page_title: 'Home',
+                description: 'Home Page'
+            },
+        },
+        {
+            name: 'about',
+            template: 'src/pages/about/about.hbs',
+            output: 'dist/pages/about.html',
+            jsEntry: 'src/pages/about/about.js',
+            content: {
+                title: 'My interviewMap | About',
+                page_title: 'About',
+                description: 'About Page'
+            },
+        },
+        {
+            name: 'test',
+            template: 'src/pages/test/test.hbs',
+            output: 'dist/pages/test.html',
+            jsEntry: '',
+            content: {
+                title: 'My interviewMap | Test',
+                page_title: 'Test',
+                description: 'Test Page'
+            },
+        }
+    ]
 };
 
 
