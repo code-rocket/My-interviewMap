e12c3b05e2f6e63c0d062ac648492af5cb012647
xilanhuadechuntian <Lensgcx@163.com>
2018-09-17 21:58:19 +0800

    webpack.prod.conf
    webpack.prod.conf

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -18,8 +18,8 @@ const prodWebpackConfig = {
     mode: 'production', // 通过 mode 声明生产环境
     output: {
         path: config.build.assetsRoot,
-        filename: utils.assetsPath('js/[name].[chunkhash:8].js'),
-        chunkFilename: utils.assetsPath('js/[name].[chunkhash:8].js'),
+        filename: utils.assetsPath('js/[name].[contenthash:8].js'),
+        chunkFilename: utils.assetsPath('js/[name].[contenthash:8].js'),
         publicPath: config.build.assetsPublicPath
     },
     devtool: 'cheap-module-eval-source-map',
@@ -115,6 +115,46 @@ const prodWebpackConfig = {
     },
 
 };
+
+
+/**
+ * 开启 gzip
+ * 打包后你生成的文件就会包含一个xx.xx.gz二进制格式的压缩文件
+ * 但是我们打包后引用的是xx.xx.js，并不是这个.gz的压缩文件，需要后端配合修改一下nginx的配置，
+ * 增加gzip_static on。所以仅仅有gzip on是不行滴。
+ * ================================================
+ * Request Header ：
+ *      Accept-Encoding: gzip, deflate, br
+ * Response Header ：
+ *      Content-Encoding: gzip
+ * ================================================
+ * 注意: compression-webpack-plugin 如果是1.X的版本，则参数asset 存在。如果是2.x的版本，那么参数 asset必须换成 filename
+ * 参考：
+ * https://github.com/webpack-contrib/compression-webpack-plugin
+ * http://www.css88.com/doc/webpack2/plugins/compression-webpack-plugin/
+ */
+if (config.build.productionGzip) {
+    const CompressionWebpackPlugin = require('compression-webpack-plugin');
+    prodWebpackConfig.plugins.push(
+        new CompressionWebpackPlugin(
+            {
+                //目标资源名称。 [file] 会被替换成原始资源。[path] 会被替换成原始资源的路径， [query] 会被替换成查询字符串。默认值是 "[path].gz[query]"。
+                filename: '[path].gz[query]',
+                //可以是 function(buf, callback) 或者字符串。对于字符串来说依照 zlib 的算法(或者 zopfli 的算法)。默认值是 "gzip"。
+                algorithm: 'gzip',
+                //所有匹配该正则的资源都会被处理。默认值是全部资源。
+                test: new RegExp(
+                    '\\.(' +
+                    config.build.productionGzipExtensions.join('|') +
+                    ')$'
+                ),
+                //只有大小大于该值的资源会被处理。单位是 bytes。默认值是 0。
+                threshold: 10240,
+                //只有压缩率小于这个值的资源才会被处理。默认值是 0.8。
+                minRatio: 0.8,
+            })
+    )
+}
 
 //打包情况概览插件调用
 if (config.build.bundleAnalyzerReport) {
