916a2aa07f9754b2f8ba379cd7a3113baa0db53e
xilanhuadechuntian <Lensgcx@163.com>
2018-09-17 16:36:35 +0800

    status
    status

--- src/status.js
+++ src/status.js
@@ -2,6 +2,6 @@ export default {
     init_page: 'home',//初始化首页
     menu_status: {
         activeIndex: 0,//选中第几个一级菜单
-        isClosed: false//是否关闭左侧菜单栏
+        isClosed: true//是否关闭左侧菜单栏
     },
 };
