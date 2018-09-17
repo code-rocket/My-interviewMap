1fdc477dadaa8635d45b4e93520b75bd477a5711
xilanhuadechuntian <Lensgcx@163.com>
2018-09-17 15:58:53 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,14 +1,34 @@
-import $ from "jquery";
+import 'jquery'
 import 'bootstrap';
-import './assets/styles/main.scss';
-import './init';
-import './components/page-title/page-title';
+import Cookies from 'js-cookie';
+import './assets/css/main.scss';
 import './components/hamburger/hamburger';
 import './components/side-menu/side-menu';
 
+import status from "./status";
 
 $(document).ready(() => {
     console.log('main.js loaded');
+
+    const menu_status = status.menu_status;
+    // store init menu status
+    initRegister('menu_status', menu_status);
+
+    if (status.menu_status.isClosed) {
+        $('#wrapper').addClass('toggled');
+    }
 });
 
 
+/**
+ * init register
+ * @param key
+ * @param value
+ * @returns {boolean}
+ */
+function initRegister(key, value) {
+    const old = Cookies.get(key);
+    if (old) return false;
+    // store init menu status
+    Cookies.set(key, value);
+}
