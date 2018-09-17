0359d9a5fa37f4935953696c65c2571215a8c5d2
xilanhuadechuntian <Lensgcx@163.com>
2018-09-17 16:37:39 +0800

    hamburger
    hamburger

--- src/components/hamburger/hamburger.js
+++ src/components/hamburger/hamburger.js
@@ -1,49 +1,31 @@
-import Cookies from 'js-cookie';
-import status from "../../status";
-
 $(document).ready(() => {
     console.log('hamburger works');
 
     //hamburger dom
     let trigger = $('.hamburger'), overlay = $('.overlay');
-
-    //init, add status class to hamburger
-    hamburger_cross('init');
+    let isClosed = true;
 
     /**
      * hamburger component click fn
      */
     trigger.on('click', function () {
-        hamburger_cross('click');
+        hamburger_cross();
     });
-
 
     /**
      * hamburger cross change
-     * @param type
      */
-    function hamburger_cross(type) {
-        let menu_status = JSON.parse(Cookies.get('menu_status'));
-        let isClosed = menu_status.isClosed;
-        if (type !== 'init') {
-            isClosed = !isClosed;
-            menu_status.isClosed = isClosed;
-            // store init menu status
-            Cookies.set('menu_status', menu_status);
-        }
+    function hamburger_cross() {
         if (isClosed === true) {
             trigger.removeClass('is-open');
             trigger.addClass('is-closed');
-            overlay.hide();
-        }
-        else {
+            isClosed = false;
+        } else {
             trigger.removeClass('is-closed');
             trigger.addClass('is-open');
-            overlay.show();
+            isClosed = true;
         }
-
     }
-
 
     //wrapper chaneg
     $('[data-toggle="offcanvas"]').click(function () {
