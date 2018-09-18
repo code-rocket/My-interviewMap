36dea680146449d16e55eb0c755d6de9e1dfe6c0
xilanhuadechuntian <Lensgcx@163.com>
2018-09-18 14:16:26 +0800

    JS-utils
    JS-utils

---
+++ src/pages/JS-utils/JS-utils.js
@@ -0,0 +1,18 @@
+import {judgeType, isEqual} from "../../utils";
+
+{
+    console.log(isEqual(0, 0));// true
+    console.log(isEqual(0, -0));// false
+    console.log(isEqual(NaN, NaN)); // true
+    console.log(isEqual(Number(NaN), Number(NaN))); // true
+    console.log(isEqual('Curly', new String('Curly'))); // true
+    console.log(isEqual([1], [1])); // true
+    console.log(isEqual({value: 1}, {value: 1})); // true
+
+    let a, b;
+    a = {foo: {b: {foo: {c: {foo: null}}}}};
+    b = {foo: {b: {foo: {c: {foo: null}}}}};
+    a.foo.b.foo.c.foo = a;
+    b.foo.b.foo.c.foo = b;
+    console.log(isEqual(a, b));// true
+}
