14081e2be7fec369bb659545410083a0475dedbe
xilanhuadechuntian <Lensgcx@163.com>
2018-09-19 09:20:27 +0800

    build
    build

--- build/build.js
+++ build/build.js
@@ -40,7 +40,7 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
         console.log(chalk.cyan('  Build complete.\n'));
         console.log(chalk.yellow(
             '  Tip: built files are meant to be served over an HTTP server.\n' +
-            '  Opening about.hbs over file:// won\'t work.\n'
+            '  Opening index.html over file:// won\'t work.\n'
         ));
     });
 });
