26ef27b1b0692af465db7e41f815a9db601d7aa0
xilanhuadechuntian <Lensgcx@163.com>
2018-09-19 09:21:32 +0800

    about
    about

---
+++ src/pages/about/index.js
@@ -0,0 +1,13 @@
+/**
+ * -----------------------
+ * for about page
+ * -----------------------
+ */
+
+import './index.scss'
+
+$(document).ready(() => {
+    console.log('about page works');
+});
+
+
